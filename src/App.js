3518c3e24488becc1af3bd26ad66e0e7cf08f416
kuntalkumar <kuntalkumar789@gmail.com>
2024-10-01 09:44:40 +0530

    react-leaflet

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import MapWithMarkers from './components/MapWithMarker.jsx'
+
+const App = () => {
+  return (
+    <div>
+      <MapWithMarkers/>
+    </div>
+  )
+}
+
+export default App
